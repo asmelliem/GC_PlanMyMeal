fc80326ace0e9b053e089e847b04315c92cfdfbe
asmelliem <asmelliem@gmail.com>
2020-11-23 21:51:52 -0500

    Initialize Repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GC_PlanMyMeal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

39c3d5aa66fa93f1f405dcac890d20d089cfc5a9
Bernard Polynice <bepoly.nice@gmail.com>
2020-12-06 16:02:35 -0500

    fixed appconfiguration for cloud

--- Program.cs
+++ Program.cs
@@ -24,9 +24,8 @@ namespace GC_PlanMyMeal
               {
                   config.SetBasePath(webHostBuilderContext.HostingEnvironment.ContentRootPath)
                      .AddUserSecrets(typeof(Program).Assembly)
-                     .AddEnvironmentVariables()
-                     .AddJsonFile("appsettings.json")
-                     .AddJsonFile($"appsettings.{webHostBuilderContext.HostingEnvironment.EnvironmentName}.json");
+                     .AddEnvironmentVariables();
+                    
                   var intermediate = config.Build();
                   config.AddAzureAppConfiguration(options =>
                   {

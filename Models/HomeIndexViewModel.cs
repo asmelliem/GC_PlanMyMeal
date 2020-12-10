bece1d218d8742237bf61a0d138e3cf40455a1b9
Elizabeth Hankins <elizabethhankins@Elizabeths-MacBook-Pro.local>
2020-12-09 21:34:16 -0500

    search receipe intolerances

--- Models/HomeIndexViewModel.cs
+++ Models/HomeIndexViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace GC_PlanMyMeal.Models
 {
     public class HomeIndexViewModel
     {
         public string Diet { get; set; }
-        public string Intolerances { get; set; }
+        public string Intolerances{ get; set; }
         public int MaxCalorie { get; set; }
         public int MaxCarb { get; set; }
         public int MaxProtein { get; set; }
---
+++ Models/Intolerances.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace GC_PlanMyMeal.Models
+{
+    public class Intolerances
+    {
+        public string egg { get; set; }
+        public string dairy { get; set; }
+        public string gluton { get; set; }
+        public string grain { get; set; }
+        public string peanut { get; set; }
+        public string seafood { get; set; }
+        public string shellfish { get; set; }
+        public string soy { get; set; }
+        public string sulfite { get; set; }
+        public string treeNut { get; set; }
+        public string wheat { get; set; }
+    }
+}
